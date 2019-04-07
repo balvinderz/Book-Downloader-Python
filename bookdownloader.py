af0727d04014e547aa01d9f1a7e098baad34f894
Balvinder Singh Gambhir <balvindersi2@gmail.com>
2019-04-07 20:53:58 +0530

    Update bookdownloader.py

--- bookdownloader.py
+++ bookdownloader.py
@@ -1,13 +1,27 @@
 from tkinter import *
+from tkinter.filedialog import askdirectory
+from tkinter import messagebox
 import requests
 from bs4 import *
+import urllib.request as urllib2
+
 from functools import partial
-def download(link):
-    print(link)
+def download(link,title,extension):
+    page=requests.get(link)
+    if(len(title)>20):
+       title=title[0:19]
+    file=requests.get(link,stream=True)
+    folder = askdirectory()
+    filename=folder+'/'+title+"."+extension
+    response=urllib2.urlopen(link)
+    file_=open(filename,'wb')
+    data=response.read()
+    file_.write(data)
+    file.close()
+    messagebox.showinfo("download", "download completed")
 def scrape():
     
     bookname=e1.get()
-    print(bookname)
     url="http://libgen.is/search.php?req="+bookname+"&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def"
     page=requests.get(url)    
     soup=BeautifulSoup(page.text,'html.parser')
@@ -18,15 +32,11 @@ def scrape():
         tr=trs[i]
         author=tr.find_all('td')[1].a.text
         title=tr.find_all('td')[2].a.text
+        downloadlink=tr.find_all('td')[10].a['href']
+        extension=tr.find_all('td')[8].text
         Label(w,text=author).grid(row=i,column=1)
         Label(w,text=title).grid(row=i,column=2)
-        Button(w,text='download',command=partial(download,i)).grid(row=i,column=3)
-        
-    """for i in range(1,len(trs)):
-        soup=trs[i]
-        print(soup)
-    for i in range(5):"""
-    
+        Button(w,text='download',command=partial(download,downloadlink,title,extension)).grid(row=i,column=3)
 r=Tk()
 r.title("Book Downloader")
 w=Frame(r,)
